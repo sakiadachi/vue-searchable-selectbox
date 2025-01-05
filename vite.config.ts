67d53f6b9f40360cc140ea836cd4c09adee8c767
sakiadachi <sakiadachi.88@gmail.com>
2025-01-05 17:33:02 +0900

    fix: change package name

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'node:path'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VueSearchableSelectbox',
-      fileName: 'vue-searchable-selectbox'
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
-})
+	plugins: [vue()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "VueLightSearchableSelectbox",
+			fileName: "vue-light-searchable-selectbox",
+		},
+		rollupOptions: {
+			external: ["vue"],
+			output: {
+				globals: {
+					vue: "Vue",
+				},
+			},
+		},
+	},
+});
